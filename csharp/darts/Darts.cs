882630f425ebdeabcade819d13650ebddd66885a
gdhumal <42371324+gdhumal@users.noreply.github.com>
2021-07-03 17:34:02 +0530

    Darts
    Extra exercises

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double calculateDistance = Math.Sqrt(x * x + y * y);
+        
+        return calculateDistance <= 1 ? 10 : calculateDistance <= 5 ? 5 : calculateDistance <= 10 ? 1 : 0;
+    }
+}
