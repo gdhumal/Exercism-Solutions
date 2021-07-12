1aebc132bc910cff7960da1b93f0acc9f30be8d2
gdhumal <42371324+gdhumal@users.noreply.github.com>
2021-07-12 11:43:39 +0530

    Collatz Conjecture
    Side exercise

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        int steps = 0;
+        CalculateSteps(number, ref steps);
+        return steps;
+    }
+
+    public static int CalculateSteps(int number, ref int steps)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (number != 1)
+        {
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+            steps++;
+
+            CalculateSteps(number, ref steps);
+        }
+
+        return number;
+    }
+}
