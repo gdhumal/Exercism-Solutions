ecac4cd1ce07ef3eb76c2c26f04754a82aa5b79a
gdhumal <42371324+gdhumal@users.noreply.github.com>
2021-07-13 16:01:02 +0530

    Sum Of Multiples
    Side exercise

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var multiplesWithOutZero = multiples.Where(x => x != 0);
+        return Enumerable.Range(0, max).Where(i => multiplesWithOutZero.Any(m => i % m == 0)).Sum();
+    }
+}
